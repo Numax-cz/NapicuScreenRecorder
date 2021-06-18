a37fda8fe5cb7058a994643efb4481734dc8ed41
Marcel <marcel.mikolas@gmail.com>
2021-06-18 21:30:44 +0200

    App

--- src/render.js
+++ src/render.js
@@ -17,7 +17,7 @@ async function getVideoSources() {
     });
 
     const videoOptionsMenu = Menu.buildFromTemplate(
-        inputSources.map(source => {
+        InputVideo.map(source => {
             return {
                 label: source.name,
                 click: () => selectSource(source)
@@ -26,5 +26,47 @@ async function getVideoSources() {
     );
     videoOptionsMenu.popup();
 }
+var Record;
+const Chunks = [];
 
+async function selectSource(source) {
+
+
+    const con = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    const stream = await navigator.mediaDevices
+        .getUserMedia(con);
+    video.srcObject = stream;
+    video.play();
+
+
+    const option = { mimeType: 'video/webm; codecs=h264' };
+    Record = new MediaRecorder(stream, option);
+    Record.ondataavailable = DataAv;
+    Record.onstop = InStop;
+}
+
+function DataAv(e) {
+    Chunks.push(e.data)
+}
+
+async function InStop(e) {
+
+}
+//Other Buttons
+
+
+start.onclick = e => {
+
+
+
+}
 
