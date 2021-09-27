6abdef339286fd2ede8d7c933f8f7346c4804f4c
Marcel <numax.cz@protonmail.ch>
2021-09-27 21:13:01 +0200

    Úprava dokumentace

--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 # NapicuScreenRecorder
-* Aplikace pro nahrávání obrazu
-* Pro nahrávání zvuku potřeba povolit v `render.js` (Na linuxu problém se zvukem. Více na [Electron - DesktopCapturer](https://www.electronjs.org/docs/api/desktop-capturer) )
-* Aplikace je připravena pro architekturu x64, lze ji ale změnit v `package.json` více na [Electron Builder - Any Target](https://www.electron.build/configuration/linux)
-* Vyzkoušeno na rozlišení 1920x1080
-* Kodek vp9
-* Výstup .webm
-* Vytvořeno pomocí [Electron](https://www.electronjs.org/)
 
+- Aplikace pro nahrávání obrazu
+- Pro nahrávání zvuku potřeba povolit v `render.js` (Na linuxu problém se zvukem. Více na [Electron - DesktopCapturer](https://www.electronjs.org/docs/api/desktop-capturer) )
+- Aplikace je připravena pro architekturu x64, lze ji ale změnit v `package.json` více na [Electron Builder - Any Target](https://www.electron.build/configuration/linux)
+- Vyzkoušeno na rozlišení 1920x1080
+- Kodek vp9
+- Výstup .webm
+- Vytvořeno pomocí [Electron](https://www.electronjs.org/)
+- Inspirováno [Fireship-electron-screen-recorder](https://github.com/fireship-io/223-electron-screen-recorder)
 
 ### Instalace
+
+- Potřeba [NodeJS](https://nodejs.org/en/) >= 12.13.0
+- Potřeba [Git](https://git-scm.com/)
+
 1. Naklonování repozitáře
    ```sh
    git clone https://github.com/Numax-cz/NapicuScreenRecorder.git
@@ -18,22 +23,31 @@
    npm install
    ```
 3. Spuštění aplikace
-    ```sh
-    npm run start
-    ```
+   ```sh
+   npm run start
+   ```
 
 ### Buildnutí
-* Pro Linux x64 - snap
-   ```sh
-   npm run buildLin
-   ```
-* Pro Windows x64
-   ```sh
-   npm run buildWin
-   ```
-### Pack
-   ```sh
-   npm run pack
-   ```
 
-![Nahled](https://github.com/Numax-cz/NapicuScreenRecorder/blob/main/img/Screen.png)
+- Pro Linux x64 - snap
+  ```sh
+  npm run buildLin
+  ```
+- Pro Windows x64
+  ```sh
+  npm run buildWin
+  ```
+
+### Pack
+
+```sh
+npm run pack
+```
+
+### Používaní
+
+1. Při zapnutí vyberete zdroj
+2. Spustíte nahrávání
+3. Stop
+4. Uložíte video kam potřebujete
+   ![Nahled](https://github.com/Numax-cz/NapicuScreenRecorder/blob/main/img/Screen.png)
--- src/render.js
+++ src/render.js
@@ -2,99 +2,82 @@ const videoSelectBtn = document.getElementById('videocct');
 videoSelectBtn.onclick = getVideoSources;
 const { desktopCapturer } = require('electron');
 const { Menu, dialog } = require('electron').remote;
-const { writeFile } = require('fs')
-const video = document.querySelector("video");
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
+const { writeFile } = require('fs');
+const video = document.querySelector('video');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
 var Record;
 const Chunks = [];
 
-
 async function getVideoSources() {
-    const InputVideo = await desktopCapturer.getSources({
-        types: ['window', 'screen']
-    });
-    const videoOptionsMenu = Menu.buildFromTemplate(
-        InputVideo.map(source => {
-            return {
-                label: source.name,
-                click: () => selectSource(source)
-            };
-        })
-    );
-    videoOptionsMenu.popup();
+  const InputVideo = await desktopCapturer.getSources({
+    types: ['window', 'screen'],
+  });
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    InputVideo.map((source) => {
+      return {
+        label: source.name,
+        click: () => selectSource(source),
+      };
+    })
+  );
+  videoOptionsMenu.popup();
 }
 
 async function selectSource(source) {
-    console.log(source);
-    const con = {
-        //--Audio--// 
-        // audio: {
-        //     chromeMediaSource: 'desktop'
-        // },
-        audio: false,
-        video: {
-            mandatory: {
-                chromeMediaSource: 'desktop',
-                chromeMediaSourceId: source.id
-            }
-        }
-    };
-    const stream = await navigator.mediaDevices
-        .getUserMedia(con);
+  console.log(source);
+  const con = {
+    //--Audio--//
+    // audio: {
+    //   chromeMediaSource: 'desktop',
+    // },
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+  const stream = await navigator.mediaDevices.getUserMedia(con);
 
+  const VideoOut = await navigator.mediaDevices.getUserMedia(con);
 
-
-    const VideoOut = await navigator.mediaDevices
-        .getUserMedia(con);
-
-
-
-
-
-
-    video.srcObject = stream;
-    video.play();
-    const option = { mimeType: 'video/webm;codecs=vp9' };
-    Record = new MediaRecorder(VideoOut, option);
-    Record.ondataavailable = Data;
-    Record.onstop = Save;
+  video.srcObject = stream;
+  video.play();
+  const option = { mimeType: 'video/webm;codecs=vp9' };
+  Record = new MediaRecorder(VideoOut, option);
+  Record.ondataavailable = Data;
+  Record.onstop = Save;
 }
 
-
-
-
-
-
-
-
 function Data(e) {
-    Chunks.push(e.data)
+  Chunks.push(e.data);
 }
 
 async function Save(e) {
-    const blob = new Blob(Chunks, {
-        type: 'video/webm;codecs=vp9'
-    });
-    const buffer = Buffer.from(await blob.arrayBuffer());
-    const { filePath } = await dialog.showSaveDialog({
-        buttonLabel: 'Save',
-        defaultPath: `video-${Date.now()}.webm`
-    });
-    console.log(filePath);
-    writeFile(filePath, buffer, () => console.log("Uloženo"));
+  const blob = new Blob(Chunks, {
+    type: 'video/webm;codecs=vp9',
+  });
+  const buffer = Buffer.from(await blob.arrayBuffer());
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save',
+    defaultPath: `video-${Date.now()}.webm`,
+  });
+  console.log(filePath);
+  writeFile(filePath, buffer, () => console.log('Uloženo'));
 }
 
-start.onclick = e => {
-    if (Record) {
-        Record.start();
-        start.innerText = 'Nahráváš!';
-    }
+start.onclick = (e) => {
+  if (Record) {
+    Record.start();
+    start.innerText = 'Nahráváš!';
+  }
 };
 
-stop.onclick = e => {
-    if (Record) {
-        Record.stop();
-        start.innerText = 'Nahrávat';
-    }
+stop.onclick = (e) => {
+  if (Record) {
+    Record.stop();
+    start.innerText = 'Nahrávat';
+  }
 };
